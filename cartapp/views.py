117807e2796139c511c5e3075bf78204a4528359
SylviaC1234 <SylviaC432@gmail.com>
2026-02-24 11:39:09 +0300

    new commit

--- cartapp/views.py
+++ cartapp/views.py
@@ -5,3 +5,7 @@ def index(request):
     return render(request,'index.html')
 def gallery(request):
     return render(request,'gallery.html')
+
+def services(request):
+    return render(request,'services.html')
+
